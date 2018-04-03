8535eb2535f0636e066be6ec05a248564b2e9db8
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-03 10:20:06 -0700

    [UPDATE] Need to mock in unit test to make tests happy

--- src/main/java/com/example/nycseca/controllers/UsersController.java
+++ src/main/java/com/example/nycseca/controllers/UsersController.java
@@ -1,8 +1,16 @@
 package com.example.nycseca.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
+
+    @GetMapping("/")
+    public String findAllUsers() {
+        return "It's working!!!";
+    }
+
+
 
 }
