067d461244fb603a02d0d803783782b8f2ec2b11
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-03 04:43:32 -0700

    [WIP] Added tests to UsersApiFeatureTest

--- src/test/java/com/example/nycseca/features/UsersApiFeatureTest.java
+++ src/test/java/com/example/nycseca/features/UsersApiFeatureTest.java
@@ -1,12 +1,22 @@
 package com.example.nycseca.features;
 
+import com.example.nycseca.models.User;
 import com.example.nycseca.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -23,6 +33,86 @@ public class UsersApiFeatureTest {
     @After
     public void tearDown() {
         userRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldAllowFullCrudForAUser() throws Exception {
+
+        User firstUser = new User(
+                "someone",
+                "Ima",
+                "Person"
+        );
+
+        User secondUser = new User(
+                "someone_else",
+                "Someone",
+                "Else"
+        );
+
+        Stream.of(firstUser, secondUser)
+                .forEach(user -> {
+                    userRepository.save(user);
+                });
+
+        // Test get all Users
+        when()
+                .get("http://localhost:8080/users")
+        .then()
+                .statusCode(is(200))
+                .and().body(containsString("someone"))
+                .and().body(containsString("Else"));
+
+        // Test creating a User
+        User userNotYetInDb = new User(
+                "new_user",
+                "Not",
+                "Yet Created"
+        );
+
+        given()
+                .contentType(JSON)
+                .and().body(userNotYetInDb)
+        .when()
+                .post("http://localhost:8080/users")
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("new_user"));
+
+        // Test get all Users
+        when()
+                .get("http://localhost:8080/users/")
+        .then()
+                .statusCode(is(200))
+                .and().body(containsString("someone"))
+                .and().body(containsString("Else"))
+                .and().body(containsString("Yet Created"));
+
+        // Test finding one user by ID
+        when()
+                .get("http://localhost:8080/users/" + secondUser.getId())
+        .then()
+                .statusCode(is(200))
+                .and().body(containsString("Someone"))
+                .and().body(containsString("Else"));
+
+        // Test updating a user
+        secondUser.setFirstName("changed_name");
+
+        given()
+                .contentType(JSON)
+                .and().body(secondUser)
+        .when()
+                .patch("http://localhost:8080/users/" + secondUser.getId())
+        .then()
+                .statusCode(is(200))
+                .and().body(containsString("changed_name"));
+
+        // Test deleting a user
+        when()
+                .delete("http://localhost:8080/users/" + secondUser.getId())
+        .then()
+                .statusCode(is(200));
     }
 
 }
