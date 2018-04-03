155f0bccb3e1bae37735079ce3011bde1875f84c
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-03 09:57:47 -0700

    [NEW] Created UsersController and UsersControllerTest files:

---
+++ src/test/java/com/example/nycseca/controllers/UsersControllerTest.java
@@ -0,0 +1,10 @@
+import com.example.nycseca.controllers.UsersController;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UsersController.class)
+public class UsersControllerTest {
+
+}
